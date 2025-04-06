c75927b0fc6d31ed9f64993685e4b618e2d34d27
David Christian <david.w.christian@gmail.com>
2025-04-06 07:41:09 -0700

    Added community package

---
+++ packages/community/src/index.ts
@@ -0,0 +1,19 @@
+export {
+	defaultPrompt,
+	defaultPromptParams,
+	softwareEngineerTypeScriptPrompt,
+	softwareEngineerTypeScriptPromptParams,
+} from './system-prompts';
+
+export {
+	pressTheNukeButtonTool,
+	pressTheNukeButtonParams,
+	getDirectoryStructureTool,
+	getDirectoryStructureToolParams,
+	getWeatherDataTool,
+	getWeatherDataToolParams,
+	addDocumentVectorStoreTool,
+	addDocumentVectorStoreToolParams,
+	retrieveDocumentsVectorStoreTool,
+	retrieveDocumentsVectorStoreToolParams,
+} from './tools';
