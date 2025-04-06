eab6daeec18ffcd106f19b6f794681626cbe31b6
David Christian <david.w.christian@gmail.com>
2025-04-05 19:51:59 -0700

    Added Tool and SystemPrompt to sparrow package

--- packages/sparrow/src/index.ts
+++ packages/sparrow/src/index.ts
@@ -1,2 +1,4 @@
 export { Agent, type Settings } from '@sparrowstack/agent';
 export { Model, ApiKey, Provider } from '@sparrowstack/core';
+export { Tool, type IToolParams } from '@sparrowstack/tool';
+export { SystemPrompt, type ISystemPromptParams } from '@sparrowstack/system-prompt';
